74ad7eaceefcb532b304f0ee27d04ea5606837c7
evanwiseman <evanwiseman2000@gmail.com>
2026-07-08 23:04:27 -0700

    load .env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Failed to load .env: %v", err)
+	}
+
 	log.Println("Starting Bag of Holding...")
 }
