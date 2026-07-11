686d1a985464dc3e94b8339975fccb45fcef1a31
evanwiseman <evanwiseman2000@gmail.com>
2026-07-10 22:18:48 -0700

    add server functionality

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/joho/godotenv"
 )
 
+const (
+	serverPort = "8080"
+)
+
 func main() {
+	log.Println("Starting Bag of Holding...")
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Failed to load .env: %v", err)
 	}
 
-	log.Println("Starting Bag of Holding...")
+	serveMux := http.NewServeMux()
+	server := http.Server{
+		Handler: serveMux,
+		Addr:    ":" + serverPort,
+	}
+	log.Fatal(server.ListenAndServe())
 }
